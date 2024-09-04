f0ff80c0d8002376952c726c3f6dcfbd553d6bdc
DDonny <63115678+DDonnyy@users.noreply.github.com>
2024-09-04 20:15:03 +0300

    v0.2.0 initial commit

---
+++ src/objectnat/_api.py
@@ -0,0 +1,14 @@
+from .methods.adjacency_matrix import get_adjacency_matrix
+from .methods.balanced_buildings import get_balanced_buildings
+from .methods.cluster_points_in_polygons import get_clusters_polygon
+from .methods.coverage_zones import get_isochrone_zone_coverage, get_radius_zone_coverage
+from .methods.demands import get_demands
+from .methods.isochrones import get_accessibility_isochrones
+from .methods.osm_graph import get_intermodal_graph_from_osm
+from .methods.provision import NoOsmIdException, NoWeightAdjacencyException, get_provision
+from .methods.visibility_analysis import (
+    calculate_visibility_catchment_area,
+    get_visibilities_from_points,
+    get_visibility,
+    get_visibility_accurate,
+)
--- src/objectnat/methods/isochrones.py
+++ src/objectnat/methods/isochrones.py
@@ -1,34 +1,24 @@
 import geopandas as gpd
 import networkx as nx
-from dongraphio import DonGraphio, GraphType
+import numpy as np
+import pandas as pd
+from scipy.spatial import KDTree
 from shapely import Point
+from loguru import logger
+from shapely.ops import unary_union
 
 
 def get_accessibility_isochrones(
     points: Point | gpd.GeoSeries,
-    graph_type: list[GraphType],
     weight_value: int,
     weight_type: str,
-    city_graph: nx.MultiDiGraph,
-    city_crs: int,
+    graph_nx: nx.Graph,
 ) -> tuple[gpd.GeoDataFrame, gpd.GeoDataFrame | None, gpd.GeoDataFrame | None]:
     """
     Calculate accessibility isochrones based on the provided city graph for the selected graph_type from point(s).
 
     Parameters
     ----------
-    points : Point or gpd.GeoSeries
-        Points from which the isochrones will be calculated.
-    graph_type : list[GraphType]
-        List of graph types to calculate isochrones for.
-    weight_value : int
-        Weight value for the accessibility calculations.
-    weight_type : str
-        The type of the weight, could be either "time_min" or "length_meter".
-    city_graph : nx.MultiDiGraph
-        The graph representing the city.
-    city_crs : int
-        The CRS (Coordinate Reference System) for the city.
 
     Returns
     -------
@@ -37,30 +27,98 @@ def get_accessibility_isochrones(
 
     Examples
     --------
-    >>> import networkx as nx
-    >>> import geopandas as gpd
-    >>> from shapely.geometry import Point
-    >>> from dongraphio import GraphType
 
-    >>> # Create a sample city graph or download it from osm with get_intermodal_graph_from_osm()
-    >>> city_graph = nx.MultiDiGraph()
-
-    >>> # Define parameters
-    >>> graph_type = [GraphType.PUBLIC_TRANSPORT, GraphType.WALK]
-    >>> points = gpd.GeoSeries([Point(0, 0)])
-    >>> weight_value = 15
-    >>> weight_type = "time_min"
-    >>> city_crs = 4326 # Should be the same with CRS of the city graph
-
-    >>> # Calculate isochrones
-    >>> isochrones, routes, stops = get_accessibility_isochrones(
-    ...     graph_type, points, weight_value, weight_type, city_graph, city_crs
-    ... )
-
-    >>> print(isochrones)
-    >>> print(routes)
-    >>> print(stops)
     """
-    dongraphio = DonGraphio(city_crs)
-    dongraphio.set_graph(city_graph)
-    return dongraphio.get_accessibility_isochrones(graph_type, points, weight_value, weight_type)
+
+    nodes_with_data = list(graph_nx.nodes(data=True))
+    logger.info("Calculating isochrones distances...")
+    coordinates = np.array([(data["x"], data["y"]) for node, data in nodes_with_data])
+    tree = KDTree(coordinates)
+
+    if isinstance(points,Point):
+        points = [points]
+
+    target_coord = [(p.x, p.y) for p in points]
+    distance, indices = tree.query(target_coord)
+
+    nearest_nodes = [(index, nodes_with_data[idx][0]) for index, idx in enumerate(indices)] # TODO use get_closest_nodes from IduEdu
+
+    dist_nearest = pd.DataFrame(data=distance, index=pd.MultiIndex.from_tuples(nearest_nodes), columns=["dist"])
+    walk_speed = 5 * 1000 / 60 # TODO Скорость из графа взять, если есть
+    dist_nearest = dist_nearest / walk_speed if weight_type == "time_min" else dist_nearest
+
+    if (dist_nearest > weight_value).all().all():
+        raise RuntimeError(
+            "The point(s) lie further from the graph than weight_value, it's impossible to "
+            "construct isochrones. Check the coordinates of the point(s)/their projection"
+        )
+
+    data = []
+    for point_index, source in nearest_nodes:
+        dist, path = nx.single_source_dijkstra(
+            graph_nx, source, weight=weight_type, cutoff=weight_value
+        )
+        for target_node, way in path.items():
+            source = way[0]
+            distance = dist.get(target_node, np.nan)
+            data.append((point_index, source, target_node, distance))
+
+    dist_matrix = pd.DataFrame(data, columns=["point_index", "source", "destination", "distance"])
+    dist_matrix = dist_matrix.pivot_table(index=["point_index", "source"], columns="destination", values="distance")
+
+    dist_matrix = dist_matrix.add(dist_nearest.dist, axis=0).transpose()
+    dist_matrix = dist_matrix.mask(dist_matrix >= weight_value, np.nan)
+    dist_matrix.dropna(how="all", inplace=True)
+
+    results = []
+    point_num = 0
+    logger.info("Building isochrones geometry...")
+
+    for unique_ind, node_ind in dist_matrix.columns:
+        geometry = []
+        for ind in dist_matrix.index:
+            value = dist_matrix.loc[ind, (unique_ind, node_ind)]
+            if not pd.isna(value):
+                node = graph_nx.nodes[ind]
+                point = Point(node["x"], node["y"])
+                geometry.append(
+                    point.buffer(round(weight_value - value, 2) * walk_speed * 0.8)
+                    if weight_type == "time_min"
+                    else point.buffer(round(weight_value - value, 2) * 0.8)
+                )
+        geometry = unary_union(geometry)
+        results.append({"geometry": geometry, "point": str(points.iloc[point_num]), "point_number": point_num})
+        point_num += 1
+
+    isochrones = gpd.GeoDataFrame(data=results, geometry="geometry", crs=city_crs)
+    # isochrones["travel_type"] = str([d.russian_name for d in graph_type])
+    isochrones["weight_type"] = weight_type
+    isochrones["weight_value"] = weight_value
+    stops, routes = (None, None)
+    if GraphType.PUBLIC_TRANSPORT in self.graph_type and self.weight_type == "time_min":
+        node_data = {
+            node: mobility_graph.nodes[node]
+            for node in dist_matrix.index
+            if mobility_graph.nodes[node]["stop"] == "True"
+        }
+        if len(node_data) > 0:
+            logger.info("Building public transport geometry...")
+            stops, routes = self._get_routes(node_data, mobility_graph)
+        else:
+            logger.info("No public transport nodes in accessibility")
+    return isochrones, routes, stops
+
+
+def _get_routes(self, stops, mobility_graph):
+    stops = pd.DataFrame(stops).T
+    stops["geometry"] = stops.apply(lambda x: Point(x.x, x.y), axis=1)
+    stops.drop(columns=["x", "y"], inplace=True)
+
+    subgraph = mobility_graph.subgraph(stops.index)
+    routes = pd.DataFrame.from_records([e[-1] for e in subgraph.edges(data=True)])
+    if routes.empty:
+        return None, None
+    routes["geometry"] = routes["geometry"].apply(lambda x: from_wkt(str(x)))
+    routes_result = gpd.GeoDataFrame(data=routes, geometry="geometry", crs=self.city_crs)
+    stops_result = gpd.GeoDataFrame(data=stops, geometry="geometry", crs=self.city_crs)
+    return stops_result, routes_result
